25d05790203bd16244bd902d35d8f9a6c28ddc53
hassaammgl <h455a4mmehtab@gmail.com>
2025-03-24 02:09:18 +0500

    add Features component with interactive BentoCards and tilt effect; update App layout to include Features section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
+import Features from './components/Features'
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <main className='relative min-h-screen w-screen overflow-x-hidden '>
       <Navbar />
       <Hero />
       <About />
-      <section className='z-0 min-h-screen bg-blue-500'></section>
+      <Features />
+      <section className='z-0 min-h-screen bg-blue-500' />
     </main>
   )
 }
